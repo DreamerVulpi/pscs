171829a2ba817fa3cb777a486b027446e5c47a08
DreamerVulpi <dreamervulpi@gmail.com>
2021-05-02 14:29:58 +0300

    First version UI without table.

---
+++ Debugger/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.IO;
+namespace PsCsWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public class Phone
+    {
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public int Price { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Phone> phonesList = new List<Phone>
+            {
+                new Phone { Title="iPhone 6S", Company="Apple", Price=54990 },
+                new Phone {Title="Lumia 950", Company="Microsoft", Price=39990 },
+                new Phone {Title="Nexus 5X", Company="Google", Price=29990 }
+            };
+            phonesGrid.ItemsSource = phonesList;
+        }
+
+        private void load_button(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            
+            fileDialog.InitialDirectory = "c:\\";
+            fileDialog.Filter = "bin files (*.bin)|*.bin|All files (*.*)|*.*";
+            fileDialog.FilterIndex = 1;
+            fileDialog.RestoreDirectory = true;
+            
+
+            var fileContent = string.Empty;
+            
+            if (fileDialog.ShowDialog() == true)
+            {
+                var fileStream = fileDialog.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    fileContent = reader.ReadToEnd();        
+                }
+                nameFileString.Text = "Загружено: " + Path.GetFileName(fileDialog.FileName);
+            }
+
+        }
+    }
+   
+    
+   
+}
